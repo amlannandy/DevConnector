a9f579d553f8c6598916bf3addf49ce33eb2c0b5
HeWantedIndigo <amlannandy5@gmail.com>
2020-07-18 11:38:27 +0530

    Create React App. Added Landing, Login, Registration

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,53 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const { email, password } = formData;
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log('Success');
+  }
+
+  return (
+    <Fragment>
+      <h1 class="large text-primary">Sign in</h1>
+      <p class="lead"><i class="fas fa-user"></i> Sign into your Account</p>
+      <form class="form" onSubmit={onSubmit}>
+        <div class="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name='email'
+            value={email}
+            onChange={event => onChange(event)}
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name='password'
+            value={password}
+            onChange={event => onChange(event)}
+          />
+        </div>
+        <input type="submit" value="Register" className="btn btn-primary" />
+      </form>
+      <p class="my-1">
+        New here?
+      <Link to="/register">Register</Link>
+      </p>
+    </Fragment>
+  )
+}
+
+export default Login
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Developer Connector</h1>
+          <p className="lead">Create developer portfolios, share posts and collaborate with other developers</p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Register
+          </Link>
+            <Link to="/login" className="btn btn">
+              Login
+          </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Landing
